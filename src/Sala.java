a6524e907a9ddab46237d5f7ef3013a648991ecb
AlejandroHu <alejandrohu55@gmail.com>
2024-02-15 18:43:08 +0100

    (core) Añadimos los getters y setters de Sala

--- src/Sala.java
+++ src/Sala.java
@@ -6,4 +6,22 @@ public class Sala {
         this.numero = numero;
         this.butacas = new String[5][8];
     }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public Pelicula getPelicula() {
+        return pelicula;
+    }
+    public void setPelicula(Pelicula pelicula) {
+        this.pelicula = pelicula;
+    }
+    public String[][] getButacas() {
+        return butacas;
+    }
+    public void setButacas(String[][] butacas) {
+        this.butacas = butacas;
+    }
 }
