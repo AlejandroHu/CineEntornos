012aa88636936d2d16d1a0bfd5b995bf6de3e391
AlejandroHu <alejandrohu55@gmail.com>
2024-02-15 18:42:13 +0100

    (core) Añadimos los constructores de Sala

--- src/Sala.java
+++ src/Sala.java
@@ -2,4 +2,8 @@ public class Sala {
     private int numero;
     private Pelicula pelicula;
     private String [][] butacas;
+    public Sala(int numero) {
+        this.numero = numero;
+        this.butacas = new String[5][8];
+    }
 }
