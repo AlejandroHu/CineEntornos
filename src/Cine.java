1ba4fd30d9f224290346d2d17dedc4b724da4a5a
AlejandroHu <alejandrohu55@gmail.com>
2024-02-15 18:55:05 +0100

    (core) Añadimos los constructores para Cine

--- src/Cine.java
+++ src/Cine.java
@@ -3,4 +3,10 @@ public class Cine{
     private int aforo;
     private Sala[]salas;
     private int contadorPersonas;
+    public Cine(String nombre, int aforo, int numSala) {
+        this.nombre = nombre;
+        this.aforo = aforo;
+        this.salas = new Sala[numSala];
+
+    }
 }
