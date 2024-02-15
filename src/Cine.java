3743df810781effc8b08465a45a65b518691cfed
AlejandroHu <alejandrohu55@gmail.com>
2024-02-15 18:58:06 +0100

    (core) Añadimos los getters y setters para Cine

--- src/Cine.java
+++ src/Cine.java
@@ -12,4 +12,29 @@ public class Cine{
         }
 
     }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getAforo() {
+        return aforo;
+    }
+    public void setAforo(int aforo) {
+        this.aforo = aforo;
+    }
+    public Sala[] getSalas() {
+        return salas;
+    }
+    public void setSalas(Sala[] salas) {
+        this.salas = salas;
+    }
+
+    public int getContadorPersonas() {
+        return contadorPersonas;
+    }
+    public void setContadorPersonas(int contadorPersonas) {
+        this.contadorPersonas = contadorPersonas;
+    }
 }
