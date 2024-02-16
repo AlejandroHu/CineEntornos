d11a3f72dc0522c5e118dfca9abdc47f5321cbe3
AlejandroHu <alejandrohu55@gmail.com>
2024-02-16 13:24:21 +0100

    (fix)Solucionado error en main, quitamos los imports, ya que si esta en la misma carpeta con el main no hace falta poner imports de las clases

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-import Cine;
-import Pelicula;
-import Sala;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
