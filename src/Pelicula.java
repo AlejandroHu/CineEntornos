65f8418fc6b3745faaa79a5f7fa2bce4bf3ff05a
AlejandroHu <alejandrohu55@gmail.com>
2024-02-16 20:41:41 +0100

    (doc)Añadido javadoc a clase Pelicula

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,19 +1,57 @@
+/**
+ * La clase Pelicula representa una pelicula con un titulo y una duracion en minutos
+ * @author AlejandroHu
+ */
 public class Pelicula {
+    /**
+     * Atributo titulo, es un String que se utiliza para ponerle nombre a cada pelicula
+     */
     private String titulo;
+    /**
+     * Atributo duracion, int para indicar la duracion de cada pelicula
+     */
     private int duracion;
+
+    /**
+     * Construye una nueva instancia de la clase Pelicula con el título y la duración especificados
+     * @param titulo Esto es un String para indicar el titulo de la pelicula
+     * @param duracion Esto es un int para indicar la duracion de la pelicula
+     */
     public Pelicula(String titulo, int duracion) {
         this.titulo = titulo;
         this.duracion = duracion;
     }
+
+    /**
+     * Obtiene el String titulo que es el titulo de la pelicula
+     * @return el titulo de la pelicula
+     */
     public String getTitulo() {
         return titulo;
     }
+
+    /**
+     * Sirve para establecer el titulo de la pelicula, es un String de valor predeterminado,
+     * que puedes poner el nombre que quieras
+     * @param titulo tienes que introducir una cadena que no este vacia
+     */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+
+    /**
+     * Obtiene el int duracion que es la duracion de la pelicula en minutos
+     * @return es la duracion en minutos de la pelicula
+     */
     public int getDuracion() {
         return duracion;
     }
+
+    /**
+     * Sirve para establecer la duracion en minutos de la pelicula, es un int con valor predeterminado,
+     * para poner la duracion que quieras
+     * @param duracion es recomendable introducir un valor mayor a 30 y menor a 400 para que sea mas realista
+     */
     public void setDuracion(int duracion) {
         this.duracion = duracion;
     }
--- src/Sala.java
+++ src/Sala.java
@@ -1,29 +1,33 @@
+
 public class Sala {
     private int numero;
     private Pelicula pelicula;
     private String [][] butacas;
+
     public Sala(int numero) {
         this.numero = numero;
         this.butacas = new String[5][8];
     }
+
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
+
     public Pelicula getPelicula() {
         return pelicula;
     }
     public void setPelicula(Pelicula pelicula) {
         this.pelicula = pelicula;
     }
+
     public String[][] getButacas() {
         return butacas;
     }
-    public void setButacas(String[][] butacas) {
-        this.butacas = butacas;
-    }
+    public void setButacas(String[][] butacas) {this.butacas = butacas;}
     public void infoSala() {
 
         System.out.println("Sala: " + numero + ": " + pelicula.getTitulo());
